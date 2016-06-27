bc330c388881ba36d68ec42e20cf9f70720a0309
noobhey <joeltoling@live.com>
2016-06-27 16:52:51 -0600

    Update SimpleCrypto.php

--- SimpleCrypto.php
+++ SimpleCrypto.php
@@ -4,18 +4,29 @@ class SimpleCrypto {
     private $defTable = null;
     private $key = null;
     
+    //
+    //  User defined encryption key for encrypting and/or decrypting
+    //
     public function setKey($k) {
+        //  Check if key only contains alphanumeric characters
         if ( !ctype_alpha($k) ) return false;
         foreach ( str_split($k) as $k => $v ) $this->key .= dechex(ord($v));
+        //  Generate Vigenere table once key has been assigned
         $this->vTable();
     }
     
+    //
+    //  Check if text contains if characters are in the Vigenere table
+    //
     private function cleanText($t) {
         $return = null;
         foreach ( str_split($t) as $k => $v ) $return .= ( stripos($this->defTable, $v) !== false ? $v : null );
         return $return;
     }
     
+    //
+    //  Encryption process
+    //
     private function encrypt($t) {
         $defTable = str_split($this->defTable);
         $msg = str_split(strtolower($this->cleanText($t)));
@@ -29,6 +40,9 @@ class SimpleCrypto {
         return $return;
     }    
     
+    //
+    //  Decryption process
+    //
     private function decrypt($t) {
         $defTable = str_split($this->defTable);
         $msg = str_split(strtolower($this->cleanText($t)));
@@ -42,10 +56,15 @@ class SimpleCrypto {
         return $return;
     }
     
-    private function vTable() {       
+    //
+    //  Vigenere table creation process
+    //
+    private function vTable() {
+        //  Generate table using selected ascii codes that composes letters, numbers and punctuations
         foreach ( [[97, 122], [48, 57], [32, 47], [58, 64]] as $c ) {
             for ( $x=$c[0]; $x <= $c[1]; $x++ ) $this->defTable .= chr($x);
         }
+        //  Complete the Vigenere table by inserting the key in to the table
         $seq = str_split($this->defTable);
         foreach ( str_split($this->key) as $k => $v ) {
             $index = array_search($v, $seq);            
